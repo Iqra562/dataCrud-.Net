1a7d51f37e5dc23fd42f9dbaa7e8b24cc44bd9ca
Iqra562 <iqra44938@gmail.com>
2023-10-24 16:32:17 +0500

    connection created

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication10.Models;
+
+namespace WebApplication10.Data
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
